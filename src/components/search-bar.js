0b5eadc46fb633a5ae6a6132ffb7d610b5541a83
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-01-15 19:59:24 -0500

    initialized state

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+class SearchBar extends Component {
+	/*initializing state - plain JS object that exists on any component that is a class-based component - each instance of a class-based component has it's own copy of state - initialized by defining by constuctor method and set as this.state*/
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
+	render() {
+		return (
+		<div>
+			<input 
+			value={this.state.term}
+			onChange={event => this.setState({ term: event.target.value })} />
+		</div>
+		);
+	}
+}
 
 export default SearchBar;
